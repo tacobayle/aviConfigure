50f7c3f8b567d4b608ac4c2bcce68240abf6da2e
Nicolas Bayle <nicolas.bayle.it@gmail.com>
2020-11-03 17:58:13 +0000

    20201103

--- python/loopOverSeGroup.py
+++ python/loopOverSeGroup.py
@@ -4,9 +4,10 @@ import requests, json, os, yaml, sys
 #
 playbook = sys.argv[1]
 yamlFile = sys.argv[2]
-avi_credentials = sys.argv[3]
-cloud_uuid_no_access = sys.argv[4]
-ova_path = sys.argv[5]
-data_loaded = yaml.load(open(yamlFile))
+jsonFile = sys.argv[3]
+avi_credentials = sys.argv[4]
+cloud_uuid_no_access = sys.argv[5]
+ova_path = sys.argv[6]
+data_loaded = json.load(open(yamlFile))
 for item in data_loaded['serviceEngineGroup']:
     os.system('ansible-playbook {0} --extra-vars \'{{"seg":{1}}}\' --extra-vars \'{{"avi_credentials":{2}}}\' --extra-vars \'{{"cloud_uuid_no_access":{3}}}\' --extra-vars \'{{"ova_path":{4}}}\' --extra-vars @{5}'.format(playbook, json.dumps(item), avi_credentials, cloud_uuid_no_access, ova_path, yamlFile))
