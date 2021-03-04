21e6076db6f14c606102af68e00e88fd91272957
tacobayle <nicolas.bayle@gmail.com>
2021-03-04 10:35:59 +0000

    20210304

--- python/loopOverSeGroupCount.py
+++ python/loopOverSeGroupCount.py
@@ -14,6 +14,6 @@ no_access_vcenter = sys.argv[8]
 data_loaded = yaml.load(sys.argv[9])
 count = 0
 print(data_loaded)
-# for item in data_loaded['serviceEngineGroup']:
-#     os.system('ansible-playbook {0} --extra-vars \'{{"seg":{1}}}\' --extra-vars \'{{"avi_credentials":{2}}}\' --extra-vars \'{{"cloud_no_access_vcenter_uuid":{3}}}\' --extra-vars \'{{"ova_path":{4}}}\' --extra-vars \'{{"vsphere_username":{5}}}\' --extra-vars \'{{"vsphere_password":{6}}}\' --extra-vars \'{{"vsphere_server":{7}}}\' --extra-vars \'{{"no_access_vcenter":{8}}}\' --extra-vars \'{{"count":{9}}}\''.format(playbook, json.dumps(item), avi_credentials, cloud_no_access_vcenter_uuid, ova_path, vsphere_username, vsphere_password, vsphere_server, no_access_vcenter, count))
-#     count += int(item['numberOfSe'])
+for item in data_loaded['serviceEngineGroup']:
+    os.system('ansible-playbook {0} --extra-vars \'{{"seg":{1}}}\' --extra-vars \'{{"avi_credentials":{2}}}\' --extra-vars \'{{"cloud_no_access_vcenter_uuid":{3}}}\' --extra-vars \'{{"ova_path":{4}}}\' --extra-vars \'{{"vsphere_username":{5}}}\' --extra-vars \'{{"vsphere_password":{6}}}\' --extra-vars \'{{"vsphere_server":{7}}}\' --extra-vars \'{{"no_access_vcenter":{8}}}\' --extra-vars \'{{"count":{9}}}\''.format(playbook, json.dumps(item), avi_credentials, cloud_no_access_vcenter_uuid, ova_path, vsphere_username, vsphere_password, vsphere_server, no_access_vcenter, count))
+    count += int(item['numberOfSe'])
